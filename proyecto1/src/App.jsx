3173ac562eb60acf0066766a50bd7ae876d0fbf2
Ignacio Cuart Ferrari <ignaciocuart00@gmail.com>
2024-08-29 21:10:25 -0300

    v1

---
+++ proyecto1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TituloPrincial from './assets/TituloPrincipal.jsx';
+const App = () => {
+  return (
+    <>
+      <TituloPrincipal saludo="hola comision"/>
+    </>
+  )
+}
+
+export default App
---
+++ proyecto1/src/assets/TituloPrincipal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TituloPrincipal = (props) => {
+    return (
+        <h1>
+            {props.saludo}
+        </h1>
+    )
+}
+
+export default App
